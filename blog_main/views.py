f783be24947c9076b41c0a226b59e63ff16a92f3
Ansumanluckysky <trans.luckysky@gmail.com>
2024-06-01 19:02:16 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,20 @@
 
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+
 
 def home(request):
-    return render(request, 'home.html')
+    #categories=Category.objects.all()
+    featured_posts=Blog.objects.filter(is_featured=True,status='Published')
+    unfeatured_posts=Blog.objects.filter(is_featured=False, status='Published')
+    
+
+    context = {
+        #'categories':categories,
+        'featured_posts':featured_posts,
+        'unfeatured_posts':unfeatured_posts,
+
+    }
+    return render(request, 'home.html',context)
 
