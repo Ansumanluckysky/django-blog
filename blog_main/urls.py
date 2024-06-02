60464acbb73add6ce8fd6c5b22a65416aee506c4
Ansumanluckysky <trans.luckysky@gmail.com>
2024-06-02 17:36:22 +0530

    User authentication and authorization functn

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,8 +25,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('category/',include('blogs.urls')),
-    path('<slug:slug>/', BlogsView.blogs, name='blogs'),
+    path('blogs/<slug:slug>/', BlogsView.blogs, name='blogs'),
 
     # search endpoint
     path('blogs/search/', BlogsView.search, name='search'),
+
+    #register functionality
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
