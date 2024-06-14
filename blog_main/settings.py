e631ea5f602d86b2b3c150a54a3af420b5cabb08
Ansumanluckysky <trans.luckysky@gmail.com>
2024-06-15 02:09:38 +0530

    fixing deployment issues

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -136,4 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR /'media'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'Bootstrap4'
